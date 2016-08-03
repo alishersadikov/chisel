b561400a840db6fa88f77380afccb8715722259b
Alisher Sadikov <alishersadikov@gmailcom>
2016-08-02 23:01:11 -0600

    fixed a bug in ordered lists test

--- test/list_test.rb
+++ test/list_test.rb
@@ -11,7 +11,7 @@ class ListTest < Minitest::Test
 
   def test_it_converts_ordered_lists
     expected = "<ol>\n\t<li>Sushi</li>\n\t<li>Barbeque</li>\n\t<li>Mexican</li>\n</ol>"
-    actual = List.new("").or_list_to_html("* Sushi\n* Barbeque\n* Mexican")
+    actual = List.new("").or_list_to_html("1. Sushi\n2. Barbeque\n3. Mexican")
     assert_equal expected, actual
   end
 end
